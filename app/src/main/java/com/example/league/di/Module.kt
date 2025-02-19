54074b33bc5424876cbadf39b835b980def47db2
Mess <felagunt@live.ru>
2025-02-19 18:21:12 +0300

    di

--- app/src/main/java/com/example/league/di/Module.kt
+++ app/src/main/java/com/example/league/di/Module.kt
@@ -4,6 +4,7 @@ import com.example.league.data.network.KtorClient
 import com.example.league.data.network.KtorRemoteSource
 import com.example.league.data.repository.LeagueRepositoryImpl
 import com.example.league.domain.repository.LeagueRepository
+import com.example.league.domain.use_cases.GetChampionDetailsUseCase
 import com.example.league.domain.use_cases.GetChampionListUseCase
 import com.example.league.presentation.championList.ChampionListViewModel
 import io.ktor.client.engine.HttpClientEngine
@@ -22,6 +23,7 @@ val module = module {
     //factory<KtorRemoteSource> { KtorRemoteSource(get()) }
     singleOf(::LeagueRepositoryImpl).bind<LeagueRepository>()
     singleOf(::GetChampionListUseCase)
+    singleOf(::GetChampionDetailsUseCase)
     //factory<LeagueRepository> { LeagueRepositoryImpl(get()) }
     //factory { GetChampionListUseCase(get()) }
 
